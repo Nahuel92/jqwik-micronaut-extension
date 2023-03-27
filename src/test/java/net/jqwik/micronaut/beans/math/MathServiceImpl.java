395142f1695aaf24643a224f33bd6159a486edc3
Nahuel <12597182+Nahuel92@users.noreply.github.com>
2023-03-27 19:38:07 -0300

    refactor(jqwik-micronaut): Refactored class.
    Signed-off-by: Nahuel <12597182+Nahuel92@users.noreply.github.com>

--- src/test/java/net/jqwik/micronaut/beans/math/MathServiceImpl.java
+++ src/test/java/net/jqwik/micronaut/beans/math/MathServiceImpl.java
@@ -3,9 +3,7 @@ package net.jqwik.micronaut.beans.math;
 import jakarta.inject.Singleton;
 
 @Singleton
-public
-class MathServiceImpl implements MathService {
-
+public class MathServiceImpl implements MathService {
     @Override
     public Integer compute(final Integer num) {
         return num * 4;
