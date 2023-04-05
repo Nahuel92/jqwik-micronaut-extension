dd9cde219b1f985a23140f0252646dafd467b109
Nahuel <12597182+Nahuel92@users.noreply.github.com>
2023-04-05 20:03:17 -0300

    test(jqwik-micronaut): Fixed broken test.
    Signed-off-by: Nahuel <12597182+Nahuel92@users.noreply.github.com>

--- src/test/java/net/jqwik/micronaut/PropertySourceTest.java
+++ src/test/java/net/jqwik/micronaut/PropertySourceTest.java
@@ -5,7 +5,6 @@ import net.jqwik.micronaut.annotation.JqwikMicronautTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-// FIXME: This test fails to pass (the file is not found by Micronaut, leading to a "property not found" error)
 @JqwikMicronautTest(propertySources = "myprops.properties")
 class PropertySourceTest {
     @io.micronaut.context.annotation.Property(name = "foo.bar")
